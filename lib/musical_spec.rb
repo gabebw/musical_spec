a4334b10e0ffaa28b42af99d8c2c505218c04ab7
Gabe Berke-Williams <gabe@thoughtbot.com>
2011-11-26 10:40:40 -0500

    Comment.

--- lib/musical_spec.rb
+++ lib/musical_spec.rb
@@ -6,5 +6,6 @@ require "musical_spec/formatter"
 require "musical_spec/version"
 
 module MusicalSpec
+  # Create a singleton due to Bloopsaphone's thread wackiness.
   ONE_TRUE_BLOOPSAPHONE = Bloops.new
 end
